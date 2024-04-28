06bfde5591959de39851c1670843186a062aad58
sulthanrizal <surie0312@gmail.com>
2024-04-28 08:41:09 +0700

    create looping for the page DASHBOARD

--- src/@page/dashboard/contentHeader/index.js
+++ src/@page/dashboard/contentHeader/index.js
@@ -1,23 +1,29 @@
-import { IconCustomer, IconDolar, IconSave } from "./iconDashboard";
+import { DownIncrease, IconCustomer, IconDolar, IconSave, UpIncrease } from "./iconDashboard";
 
 export const ContentTop = [
     {
         icon: <IconDolar />,
-        persen: '22%',
+        persen: '+32.40%',
+        increase: <UpIncrease />,
+        bgColor: '#88E0913D',
         price: '$10,243.00',
         total: 'Total Revenue',
-        color: 'green'
+        color: '#50D1AA'
     }, {
         icon: <IconSave />,
-        persen: '22%',
+        persen: '-12.40%',
+        increase: <DownIncrease />,
+        bgColor: '#FF64713D',
         price: '1000000',
-        total: 'Total Revenue',
-        color: 'pink'
+        total: 'Total Dish Ordered',
+        color: '#FF6471'
     }, {
         icon: <IconCustomer />,
-        persen: '22%',
+        persen: '+2.40%',
+        increase: <UpIncrease />,
+        bgColor: '#88E0913D',
         price: '1000',
-        total: 'Total Revenue',
-        color: 'green'
+        total: 'Total Customer',
+        color: '#50D1AA'
     }
 ]
