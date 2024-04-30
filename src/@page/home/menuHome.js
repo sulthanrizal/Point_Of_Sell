7eb470e9d6182373df380b2a230e78b5fb218e96
sulthanrizal <surie0312@gmail.com>
2024-05-01 06:56:00 +0700

    create menu product in PAGE HOME

--- src/@page/home/menuHome.js
+++ src/@page/home/menuHome.js
@@ -11,12 +11,12 @@ export const imgHotDishes = [
         price: '$2.29',
         stock: '20 Bowls available'
     }, {
-        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        imgMenu: 'https://img.okezone.com/content/2018/09/19/298/1952691/5-restoran-makanan-khas-korea-yang-terkenal-lezat-di-jakarta-sudah-coba-f4Djk0Zfqw.jpg',
         titleMenu: 'Spicy seasoned seafood noodles',
         price: '$2.29',
         stock: '20 Bowls available'
     }, {
-        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        imgMenu: 'https://img.okezone.com/content/2018/09/19/298/1952691/5-restoran-makanan-khas-korea-yang-terkenal-lezat-di-jakarta-sudah-coba-f4Djk0Zfqw.jpg',
         titleMenu: 'Spicy seasoned seafood noodles',
         price: '$2.29',
         stock: '20 Bowls available'
