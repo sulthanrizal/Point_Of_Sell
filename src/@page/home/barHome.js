940348854aa13c30f73bff1a725192f09310673b
sulthanrizal <surie0312@gmail.com>
2024-05-01 14:02:00 +0700

    create Dami Data for the navbar HOME

--- src/@page/home/barHome.js
+++ src/@page/home/barHome.js
@@ -1,16 +1,12 @@
-import HotDishes from "./contentHome";
-
 const BarHome = [
     {
         titleBar: 'Hot Dishes',
         path: '/hotdishes',
-        element: <HotDishes />,
         status: false
     },
     {
         titleBar: 'Cold Dishes',
         path: '/colddishes',
-        element: <HotDishes />,
         status: false
     },
     {
--- src/@page/home/contentHome/index.js
+++ src/@page/home/contentHome/index.js
@@ -6,26 +6,27 @@ const HotDishes = () => {
         <Box className="container-hotdishes">
             {
                 imgHotDishes.map((item => {
-                    return (
-                        <Box className="menu-bar">
-                            <Box className="content-menu-bar">
-                                <Box className="menu-img">
-                                    <img src={item?.imgMenu} />
-                                </Box>
-                                <Box className="text-menu">
-                                    <Text className="title-menu">
-                                        {item?.titleMenu}
-                                    </Text>
-                                    <Text className="price-menu">
-                                        {item?.price}
-                                    </Text>
-                                    <Text className="stock-menu">
-                                        {item?.stock}
-                                    </Text>
-                                </Box>
-                            </Box>
-                        </Box>
-                    )
+
+                    //     return (
+                    //         <Box className="menu-bar">
+                    //             <Box className="content-menu-bar">
+                    //                 <Box className="menu-img">
+                    //                     <img src={item?.imgMenu} />
+                    //                 </Box>
+                    //                 <Box className="text-menu">
+                    //                     <Text className="title-menu">
+                    //                         {item?.titleMenu}
+                    //                     </Text>
+                    //                     <Text className="price-menu">
+                    //                         {item?.price}
+                    //                     </Text>
+                    //                     <Text className="stock-menu">
+                    //                         {item?.stock}
+                    //                     </Text>
+                    //                 </Box>
+                    //             </Box>
+                    //         </Box>
+                    //     )
                 }))
             }
         </Box>
