4d5aa34540ee62ebd2e96269e69485c28d7291d7
sulthanrizal <surie0312@gmail.com>
2024-05-02 19:02:33 +0700

    create PAGE EMAIl

---
+++ src/@page/email/index.js
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react"
+import './email.scss'
+
+const Email = () => {
+    return (
+        <Box className="container-email">
+            Email
+        </Box>
+    )
+}
+export default Email
