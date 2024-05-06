1bc390e85941c07056980e0c80c065d29563d499
sulthanrizal <surie0312@gmail.com>
2024-05-06 13:52:13 +0700

    create menu Cold Dishes in PAGE HOME

---
+++ src/@page/home/menuColdDishes/index.js
@@ -0,0 +1,37 @@
+import { Box, Text } from "@chakra-ui/react";
+import menuColdDishes from "./menuColdDishes";
+
+const MenuColdDishes = () => {
+    return (
+        <Box className="container-cold-dishes">
+            {
+                menuColdDishes.map((item) => {
+                    return (
+                        <Box className="menu-bar"
+                        // onClick={() => { setUbahPageHome(true) }}
+                        >
+                            <Box className="content-menu-bar">
+                                <Box className="menu-img">
+                                    <img src={item?.imgMenu} />
+                                </Box>
+                                <Box className="text-menu">
+                                    <Text className="title-menu">
+                                        {item?.titleMenu}
+                                    </Text>
+                                    <Text className="price-menu">
+                                        {item?.price}
+                                    </Text>
+                                    <Text className="stock-menu">
+                                        {item?.stock}
+                                    </Text>
+                                </Box>
+                            </Box>
+                        </Box>
+                    )
+                })
+            }
+        </Box>
+    )
+}
+
+export default MenuColdDishes;
