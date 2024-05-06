01faf21071ae5136c2f823d8e3f2b2c892c5f967
sulthanrizal <surie0312@gmail.com>
2024-05-06 13:54:14 +0700

    create dami data menu hot dishes

---
+++ src/@page/home/menuHotDishes/menuHotDishes.js
@@ -0,0 +1,45 @@
+const menuHotDishes = [
+    {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://img.okezone.com/content/2018/09/19/298/1952691/5-restoran-makanan-khas-korea-yang-terkenal-lezat-di-jakarta-sudah-coba-f4Djk0Zfqw.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://img.okezone.com/content/2018/09/19/298/1952691/5-restoran-makanan-khas-korea-yang-terkenal-lezat-di-jakarta-sudah-coba-f4Djk0Zfqw.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }
+]
+
+export default menuHotDishes
