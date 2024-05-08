6a874354efef02755cdf7934151d1366292c7082
sulthanrizal <surie0312@gmail.com>
2024-05-08 10:30:12 +0700

    create dami data menu soup

---
+++ src/@page/home/soup/menuSoup.js
@@ -0,0 +1,19 @@
+const menuShop = [
+    {
+        imgMenu: 'https://www.frisianflag.com/storage/app/media/uploaded-files/sup-krim-jagung.jpg',
+        titleMenu: 'Soup Cream',
+        price: '$10.00',
+        stock: '10 Soup Cream'
+    }, {
+        imgMenu: 'https://www.seriouseats.com/thmb/Gi-6GV26JfSSlqhWSCNgOGL1bAc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/15-minute-creamy-tomato-soup-vegan-recipe-hero-01_1-19875e4289354e5c981c96189f707c62.JPG',
+        titleMenu: 'Soup Cream',
+        price: '$10.00',
+        stock: '10 Soup Cream'
+    }, {
+        imgMenu: 'https://www.kuchpakrahahai.in/wp-content/uploads/2023/10/Roasted-tomato-garlic-soup.jpg',
+        titleMenu: 'Soup Cream',
+        price: '$10.00',
+        stock: '10 Soup Cream'
+    },
+]
+export default menuShop;
