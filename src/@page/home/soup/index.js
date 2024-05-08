e93dc2e1591d0e4559b7a32cb5d2914b6018ae3f
sulthanrizal <surie0312@gmail.com>
2024-05-08 10:28:55 +0700

    create Menu Soup in PAGE HOME

---
+++ src/@page/home/soup/index.js
@@ -0,0 +1,36 @@
+import { Box, Text } from "@chakra-ui/react";
+import menuShop from "./menuSoup";
+
+const Soup = () => {
+    return (
+        <>
+            {
+                menuShop.map((item) => {
+                    return (
+                        <Box className="menu-bar"
+                        // onClick={() => { setUbahPageHome(true) }}
+                        >
+                            <Box className="content-menu-bar">
+                                <Box className="menu-img">
+                                    <img src={item?.imgMenu} />
+                                </Box>
+                                <Box className="text-menu">
+                                    <Text className="title-menu">
+                                        {item?.titleMenu}
+                                    </Text>
+                                    <Text className="price-menu">
+                                        {item?.price}
+                                    </Text>
+                                    <Text className="stock-menu">
+                                        {item?.stock}
+                                    </Text>
+                                </Box>
+                            </Box>
+                        </Box>
+                    )
+                })
+            }
+        </>
+    )
+}
+export default Soup;
