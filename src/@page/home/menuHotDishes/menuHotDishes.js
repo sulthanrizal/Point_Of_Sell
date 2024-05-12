9d0796cf5a6701e1f12ba413855948bc31a6ed96
sulthanrizal <surie0312@gmail.com>
2024-05-12 11:49:17 +0700

    create dami data menu hot dishes

--- src/@page/home/menuHotDishes/menuHotDishes.js
+++ src/@page/home/menuHotDishes/menuHotDishes.js
@@ -39,6 +39,21 @@ const menuHotDishes = [
         titleMenu: 'Spicy seasoned seafood noodles',
         price: '$2.29',
         stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
+    }, {
+        imgMenu: 'https://assets.bonappetit.com/photos/5d4ddd602c815a00080f9771/1:1/w_1600,c_limit/BA-0919-Creamy-Pasta-Crispy-Mushroom-Playbook.jpg',
+        titleMenu: 'Spicy seasoned seafood noodles',
+        price: '$2.29',
+        stock: '20 Bowls available'
     }
 ]
 
